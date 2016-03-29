12d30dfe0973f2f75f87adb06e135f6ca4766c08
milletpu <milletpu@gmail.com>
2016-03-29 17:02:54 +0800

    整理规范了下代码格式

--- src/main/java/org/pujun/interp/InterpPm.java
+++ src/main/java/org/pujun/interp/InterpPm.java
@@ -127,6 +127,7 @@ public class InterpPm {
         RBF_interp rbf_interp_multiquadric = new RBF_interp(pm25Points,pm25s,rbf_multiquadric);
         double[] pt = {interpLat, interpLon};
         double interpPm25 = rbf_interp_multiquadric.interp(pt);
+
         return interpPm25;
     }
 
@@ -135,6 +136,7 @@ public class InterpPm {
         RBF_interp rbf_interp_multiquadric = new RBF_interp(pm10Points,pm10s,rbf_multiquadric);
         double[] pt = {interpLat, interpLon};
         double interpPm10 = rbf_interp_multiquadric.interp(pt);
+
         return interpPm10;
     }
 
