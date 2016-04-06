50fd3b4974404f8c404c281ab34cf3f668372891
milletpu <milletpu@gmail.com>
2016-04-06 18:17:29 +0800

    制作heatmap第一步完成：可在png静态地图上画上所有城市点，结果为outimage.png

--- src/main/java/org/pujun/heatmap/HeatMap.java
+++ src/main/java/org/pujun/heatmap/HeatMap.java
@@ -9,11 +9,17 @@ import org.json.JSONObject;
 import org.opengis.feature.Feature;
 import org.pujun.interp.InterpPm;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+
+import static java.lang.Math.abs;
 
 /**
  * Created by milletpu on 16/3/31.
@@ -33,22 +39,24 @@ public class HeatMap {
         FeatureCollection fc = fj.readFeatureCollection(new FileInputStream(new File(CITY_PATH)));
         FeatureIterator iterator = fc.features();
 
+        DrawHeatMap dg = new DrawHeatMap();     //实例化 画heatmap
+        dg.init();      //初始化读入地图背景，png
+
         while (iterator.hasNext()) {
             Feature feature = iterator.next();
             String[] thisLocation = feature.getProperty("cp").getValue().toString().split(",");
             double thisLon = Double.parseDouble(thisLocation[0].replace("[", ""));
             double thisLat = Double.parseDouble(thisLocation[1].replace("]", ""));
 
-            System.out.println("lat:"+ thisLat + "lon:" + thisLon);
-            System.out.println(interpPm.pm10(thisLat, thisLon));
-            System.out.println(interpPm.pm25(thisLat, thisLon));
+            dg.drawEllipse(thisLat,thisLon);//取出全国所有城市的坐标，在地图png上画点
+
+
+//            System.out.println("lat:"+ thisLat + "lon:" + thisLon);
+//            System.out.println(interpPm.pm10(thisLat, thisLon));
+//            System.out.println(interpPm.pm25(thisLat, thisLon));
         }
-
-
     }
-}
 
-class HeatMapTest{
     public static void main(String[] args) throws IOException, ParseException {
         HeatMap hmp = new HeatMap("2013-12-18 06:00:00");
         hmp.getInterpPm();
