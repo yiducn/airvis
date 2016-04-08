da13ca839932be1392153ec82515020e29ec05b8
milletpu <milletpu@gmail.com>
2016-04-08 18:38:02 +0800

    可以画历史全国pm2.5的热力图，又可以画插值得到的全国城市pm2.5热力图。

--- src/main/java/org/pujun/heatmap/HeatMap.java
+++ src/main/java/org/pujun/heatmap/HeatMap.java
@@ -46,7 +46,7 @@ public class HeatMap {
         for (int i = 0; i < interpPm.pm25s.length; i++) {
             dg.outpng="/Users/milletpu/airvis/src/main/java/org/pujun/heatmap/outimageorigin.png";
             dg.graphics.setColor(useColor(interpPm.pm25s[i]));
-            System.out.println(interpPm.pm25s[i]);
+            //System.out.println(interpPm.pm25s[i]);
             dg.drawEllipse(interpPm.pm25Points[i][0], interpPm.pm25Points[i][1]);
         }
 
