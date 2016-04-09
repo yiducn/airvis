7fdde740d4c4835c844a85054c5c73d12c94e764
milletpu <milletpu@gmail.com>
2016-04-09 22:33:51 +0800

    增加Correlation计算相关度，和自动计算出最优的lag及其对应相关度。

--- src/main/java/org/pujun/correl/Correlation.java
+++ src/main/java/org/pujun/correl/Correlation.java
@@ -4,12 +4,14 @@ import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
 import java.util.Arrays;
 
+import static java.lang.Math.abs;
+
 /**
  * Created by milletpu on 16/4/9.
  */
 public class Correlation {
     private double pearsonsResult;
-    private double lagResult;
+    private double[] lagResult ={0,0};
 
     public double getPearsonsResult(double[] x, double[] y){
 
@@ -19,14 +21,35 @@ public class Correlation {
         return pearsonsResult;
     }
 
-    public double getLagResult(double[] x, double[] y, int lag){  //arrayY后移lag位，arrayX前端截掉，arrayY后端截掉
-        x = Arrays.copyOfRange(x, lag, x.length);
-        y = Arrays.copyOfRange(y,0,y.length-lag);
-
-        PearsonsCorrelation pearsonsCorrelation = new PearsonsCorrelation();
-        lagResult = pearsonsCorrelation.correlation(x,y);
+    /**
+     * Input two time seires then get the best lag and its corresponding correlation value
+     * @param x
+     * @param y
+     * @return LagResult[0]: best lag, lagResult[1]: correlation value
+     */
+    public double[] getLagResult(double[] x, double[] y){  //arrayY后移lag位，arrayX前端截掉，arrayY后端截掉
+        double thisLagResult = 0;
+        for (int i = 0; i < x.length/2; i++) {
+            x = Arrays.copyOfRange(x, i, x.length);
+            y = Arrays.copyOfRange(y, 0, y.length - i);
+            PearsonsCorrelation pearsonsCorrelation = new PearsonsCorrelation();
+            thisLagResult = pearsonsCorrelation.correlation(x, y);
+            if (abs(thisLagResult) > lagResult[1]){
+                lagResult[0] = i;
+                lagResult[1] = thisLagResult;
+            }
+        }
 
         return lagResult;
+    }
+
+    public static void main(String[] args) {
+        double[] y = {1,2,3,4,5,6,7,8,9,10};
+        double[] x = {12,1,8,7,6,5,4,3,2,1};
+        Correlation correlation = new Correlation();
+        System.out.println(correlation.getLagResult(x,y)[0]);
+        System.out.println(correlation.getLagResult(x,y)[1]);
+
     }
 
 }
