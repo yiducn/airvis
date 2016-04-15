911f3bd32d21b8054b21ef0596ca1226ecfe0b4d
milletpu <milletpu@gmail.com>
2016-04-15 17:29:13 +0800

    实现根据两个站点的历史数据，计算lag相关性。

--- src/main/java/org/pujun/correl/Correlation.java
+++ src/main/java/org/pujun/correl/Correlation.java
@@ -1,6 +1,13 @@
 package org.pujun.correl;
 
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.pujun.interp.InterpPm;
 
 import java.text.ParseException;
@@ -55,6 +62,91 @@ public class Correlation {
     }
 
     /**
+     * 输入两个观测站点的“编号”，观测开始时间，结束时间。
+     * 得到两个站点在此时间段内的最优lag和对应的相关度。耗时3s
+     * @param code1
+     * @param code2
+     * @param startTime
+     * @param endTime
+     * @return 返回: [0]=lag, [1]=correaltion value
+     * @throws ParseException
+     */
+    public double[] getLagCorelPM25(String code1, String code2, String startTime, String endTime) throws ParseException {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        df.setCalendar(new GregorianCalendar(new SimpleTimeZone(0, "GMT")));
+        Calendar cal = Calendar.getInstance();
+        //连接数据库
+        MongoClient client = new MongoClient("127.0.0.1", 27017);
+        DB db = client.getDB("airdb");
+        DBCollection pmCollection = db.getCollection("pm_data");
+
+        Date thisDate = df.parse(startTime);
+        Date endDate = df.parse(endTime);
+        ArrayList<Double> clusterTimeSeries = new ArrayList<Double>();
+        ArrayList<Double> codeTimeSeries = new ArrayList<Double>();
+
+        //查询cluster0的各时刻历史数据
+        cal.setTime(df.parse(startTime));
+        BasicDBObject queryCluster = new BasicDBObject();
+        double thisClusterPM25, lastClusterPM25 = 0;
+        while(thisDate.before(endDate)) {
+            queryCluster.put("time", thisDate);
+            queryCluster.put("code", code1);
+            DBCursor cur = pmCollection.find(queryCluster);
+            if (cur.hasNext()) {    //若存在此时刻的历史数据，则加入list中
+                thisClusterPM25 = Double.parseDouble(cur.next().get("pm25").toString());
+                clusterTimeSeries.add(thisClusterPM25);
+                lastClusterPM25 = thisClusterPM25;
+            } else {    //若不存在这个时间，则复制前一个时刻的数值
+                clusterTimeSeries.add(lastClusterPM25);
+            }
+
+            cal.add(Calendar.HOUR, 1);  //时间+1h循环
+            thisDate = cal.getTime();
+        }
+
+        //查询code[0]的各时刻历史数据
+        cal.setTime(df.parse(startTime));
+        BasicDBObject queryCode = new BasicDBObject();
+        thisDate = df.parse(startTime);
+        double thisCodePM25, lastCodePM25=0;
+        while(thisDate.before(endDate)) {
+            queryCode.put("time", thisDate);
+            queryCode.put("code", code2);
+            DBCursor cur = pmCollection.find(queryCode);
+            if (cur.hasNext()) {    //若存在此时刻的历史数据，则加入list中
+                thisCodePM25 = Double.parseDouble(cur.next().get("pm25").toString());
+                codeTimeSeries.add(thisCodePM25);
+                lastCodePM25 = thisCodePM25;
+            } else {    //若不存在这个时间，则复制前一个时刻的数值
+                codeTimeSeries.add(lastCodePM25);
+            }
+
+            cal.add(Calendar.HOUR, 1);  //时间+1h循环
+            thisDate = cal.getTime();
+        }
+
+        //ArrayList转数组
+        double[] clusterTimeSeriesDouble = new double[clusterTimeSeries.size()];
+        for (int i = 0; i < clusterTimeSeries.size(); i++) {
+            clusterTimeSeriesDouble[i] = clusterTimeSeries.get(i);
+            //System.out.println(clusterTimeSeries.get(i));
+        }
+        double[] codeTimeSeriesDouble = new double[codeTimeSeries.size()];
+        for (int i = 0; i < codeTimeSeries.size(); i++) {
+            codeTimeSeriesDouble[i] = codeTimeSeries.get(i);
+            //System.out.println(clusterTimeSeries.get(i));
+        }
+        //System.out.println(clusterTimeSeries.size());
+        //System.out.println(codeTimeSeries.size());
+
+        //计算lag相关性
+        double[] lagCorrelation = getLagResult(clusterTimeSeriesDouble, codeTimeSeriesDouble);
+        return lagCorrelation;
+
+    }
+
+    /**
      * 输入纬度＋经度＋起始时间＋结束时间，计算出该地点在该时间段内“每天”的pm2.5插值数据。
      * “每天”／“每小时”／可以内部调整。
      * @param lat
@@ -94,15 +186,21 @@ public class Correlation {
     }
 
     public static void main(String[] args) throws ParseException {
-        double[] x,y;
-        Correlation correlation = new Correlation();
-        x = correlation.getInterpPM25TimeSeries(40.0239, 116.2202, "2015-02-08 00:00:00", "2015-02-11 09:00:00");
-        y = correlation.getInterpPM25TimeSeries(38.1006, 114.4995, "2015-02-08 00:00:00", "2015-02-11 09:00:00");
+//        double[] x,y;
+//        Correlation correlation = new Correlation();
+//        x = correlation.getInterpPM25TimeSeries(40.0239, 116.2202, "2015-02-08 00:00:00", "2015-02-11 09:00:00");
+//        y = correlation.getInterpPM25TimeSeries(38.1006, 114.4995, "2015-02-08 00:00:00", "2015-02-11 09:00:00");
+//
+//        double[] lag = correlation.getLagResult(y, x);
+//        System.out.println(lag[0]);
+//        System.out.println(lag[1]);
+//        //1分钟
 
-        double[] lag = correlation.getLagResult(y, x);
+        Correlation correlation = new Correlation();
+        double [] lag = correlation.getLagCorelPM25("1007A","1007A","2015-03-01 00:00:00","2015-04-01 00:00:00");
         System.out.println(lag[0]);
         System.out.println(lag[1]);
-        //1分钟
+
     }
 
 
